ee648fea84777dc658d648c134b2c01f84c1d4d4
Serhii Usatiuk <serhii.usatiuk@icloud.com>
2023-11-23 23:51:12 +0100

    reconstructQueue

---
+++ reconstructQueue.js
@@ -0,0 +1,25 @@
+function reconstructQueue(input) {
+  input.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return b[0] - a[0];
+  });
+
+  const res = [];
+  for (const person of input) {
+    res.splice(person[1], 0, person);
+  }
+  return res;
+}
+
+console.log(
+  reconstructQueue([
+    [7, 0],
+    [4, 4],
+    [7, 1],
+    [5, 0],
+    [6, 1],
+    [5, 2],
+  ])
+);
